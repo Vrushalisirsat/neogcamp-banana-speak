4b4c5e05bc792e1bbce3fafa2c7bd6fa57595387
Vrushali Sirsath <95585241+Vrushalisirsat@users.noreply.github.com>
2023-04-15 23:11:48 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,4 @@
+console.log("hello")
+
+username = prompt("give me your username : ")          //give input from user
+alert("script works" + username)                       //type input and show output
